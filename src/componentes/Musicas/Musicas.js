0482d6225be7b8ce9de3ffcf8f0bb489ab8363cc
Renan Lopes <r.maruffa@gmail.com>
2023-01-04 16:02:06 -0300

    Exercicios de API II resolvidos

--- src/componentes/Musicas/Musicas.js
+++ src/componentes/Musicas/Musicas.js
@@ -1,27 +1,83 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Botao, ContainerInputs, ContainerMusicas, InputMusica, Musica } from './styled'
+import axios from 'axios'
 
-const musicasLocal = [{
-    artist: "Artista 1",
-    id: "1",
-    name: "Musica1",
-    url: "http://spoti4.future4.com.br/1.mp3"
-},
-{
-    artist: "Artista 2",
-    id: "2",
-    name: "Musica2",
-    url: "http://spoti4.future4.com.br/2.mp3"
-},
-{
-    artist: "Artista 3",
-    id: "3",
-    name: "Musica3",
-    url: "http://spoti4.future4.com.br/3.mp3"
-}]
+// const musicasLocal = [{
+//     artist: "Artista 1",
+//     id: "1",
+//     name: "Musica1",
+//     url: "http://spoti4.future4.com.br/1.mp3"
+// },
+// {
+//     artist: "Artista 2",
+//     id: "2",
+//     name: "Musica2",
+//     url: "http://spoti4.future4.com.br/2.mp3"
+// },
+// {
+//     artist: "Artista 3",
+//     id: "3",
+//     name: "Musica3",
+//     url: "http://spoti4.future4.com.br/3.mp3"
+// }]
 
 export default function Musicas(props) {
-    const [musicas, setMusicas] = useState(musicasLocal)
+    const [musicas, setMusicas] = useState([]);
+    const [name, setName] = useState("");
+    const [artist, setArtist] = useState("");
+    const [url, setUrl] = useState("");
+
+
+    useEffect(() => {
+        getPlaylistTracks()
+        // eslint-disable-next-line
+    }, []);
+
+    const getPlaylistTracks = () => {
+        axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${props.playlist.id}/tracks`,
+            { headers: { Authorization: "renan-maruffa-barbosa" } }
+        )
+            .then((response) => {
+                setMusicas(response.data.result.tracks)
+            })
+            .catch(err => console.log(err.response))
+    };
+
+    const body = {
+        name: name,
+        artist: artist,
+        url: url
+    }
+
+    const addTrackToPlaylist = () => {
+        axios.post(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${props.playlist.id}/tracks`,
+            body,
+            { headers: { Authorization: "renan-maruffa-barbosa" } }
+        )
+            .then((response) => {
+                console.log(response.data)
+                alert("Musica adicionada com sucesso!")
+                getPlaylistTracks()
+                setName("")
+                setArtist("")
+                setUrl("")
+            })
+            .catch(err => console.log(err.response));
+    };
+
+
+    const removeTrackFromPlaylist = (id) => {
+        axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${props.playlist.id}/tracks/${id}`,
+            { headers: { Authorization: "renan-maruffa-barbosa" } }
+        )
+            .then((response)=>{
+                alert("Musica deletada com sucesso!")
+                getPlaylistTracks()
+            })
+            .catch((err)=>console.log(err.response))
+    }
+
+
 
     return (
         <ContainerMusicas>
@@ -31,14 +87,14 @@ export default function Musicas(props) {
                     <Musica key={musica.id}>
                         <h3>{musica.name} - {musica.artist}</h3>
                         <audio src={musica.url} controls />
-                        <button>X</button>
+                        <button onClick={()=>removeTrackFromPlaylist(musica.id)}>X</button>
                     </Musica>)
             })}
             <ContainerInputs>
-                <InputMusica placeholder="artista" />
-                <InputMusica placeholder="musica" />
-                <InputMusica placeholder="url" />
-                <Botao>Adicionar musica</Botao>
+                <InputMusica placeholder="artista" value={artist} onChange={(e) => setArtist(e.target.value)} />
+                <InputMusica placeholder="musica" value={name} onChange={(e) => setName(e.target.value)} />
+                <InputMusica placeholder="url" value={url} onChange={(e) => setUrl(e.target.value)} />
+                <Botao onClick={addTrackToPlaylist}>Adicionar musica</Botao>
             </ContainerInputs>
         </ContainerMusicas>
     )
--- src/componentes/Playlists/Playlists.js
+++ src/componentes/Playlists/Playlists.js
@@ -1,32 +1,89 @@
-import React, {  useState } from "react";
+import React, { useEffect, useState } from "react";
 import Musicas from "../Musicas/Musicas";
+import axios from "axios"
 
-const playlistsLocal = [
-    {
-        id: 1,
-        name: "Playlist 1"
-    },
-    {
-        id: 2,
-        name: "Playlist 2"
-    },
-    {
-        id: 3,
-        name: "Playlist 3"
-    },
-    {
-        id: 4,
-        name: "Playlist 4"
-    },
-]
+// const playlistsLocal = [
+//     {
+//         id: 1,
+//         name: "Playlist 1"
+//     },
+//     {
+//         id: 2,
+//         name: "Playlist 2"
+//     },
+//     {
+//         id: 3,
+//         name: "Playlist 3"
+//     },
+//     {
+//         id: 4,
+//         name: "Playlist 4"
+//     },
+// ]
+
 function Playlists() {
-    const [playlists, setPlaylists] = useState(playlistsLocal)
-  
+    const [playlists, setPlaylists] = useState([]);
+    const [pesquisa, setPesquisa] = useState("");
+
+    useEffect(() => { getAllPlaylists() }, [])
+
+    const getAllPlaylists = () => {
+        axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists",
+            { headers: { Authorization: "renan-maruffa-barbosa" } }
+        )
+            .then((response) => {
+                setPlaylists(response.data.result.list)
+            })
+            .catch((err) => {
+                console.log(err.response);
+            })
+    };
+
+    const searchPlaylist = async (busca) => {
+        try {
+            const response = await axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/search?name=${busca}`,
+                { headers: { Authorization: "renan-maruffa-barbosa" } }
+            );
+            setPlaylists(response.data.result.playlist)
+            setPesquisa("")
+        }
+        catch (error) {
+            console.log(error.response);
+        }
+    };
+
+    const deletePlaylist = async (id) => {
+        try {
+            const response = await axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`,
+                { headers: { Authorization: "renan-maruffa-barbosa" } })
+            alert("Playlist deletada com sucesso!")
+            getAllPlaylists()
+        }
+        catch (error) {
+            alert("Playlist não encontrada")
+        }
+    }
+
+
     return (
         <div>
+
+            <input
+                placeholder="Digite o nome da Playlist"
+                value={pesquisa}
+                onChange={(e) => setPesquisa(e.target.value)}>
+            </input>
+
+            <button onClick={() => searchPlaylist(pesquisa)}>Pesquisar</button>
+            <button onClick={getAllPlaylists}>Voltar</button>
+
             {playlists.map((playlist) => {
-                return <Musicas key={playlist.id} playlist={playlist}/>
-            })}
+                return (
+                    <><Musicas key={playlist.id} playlist={playlist} />
+                        <button onClick={() => deletePlaylist(playlist.id)}>Excluir Playlist</button></>
+                )
+            })
+            }
 
         </div>
     );
